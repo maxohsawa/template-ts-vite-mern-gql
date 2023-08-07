191f28dc1849575fe4849f9d86aa145b46547931
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-07 14:20:44 -0400

    adds typedefs and mutations for user sign up and login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,22 @@
 const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
-    dummy: String
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `
 
